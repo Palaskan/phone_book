b2f1f53ebcf31b2ffd235f8f12692e262a7d3067
Carlos <onecarlostlol@gmail.com>
2023-07-17 20:39:47 +0200

    Añadidos cambios para los tests
    Añadidos tests para cuando el número de teléfono es una cadena de texto, cuando son muchos números y cuando el número es negativo.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-    validates :first_name, :phone_number, presence: true
-
-
+    validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only admits letters" }
+    validates :phone_number, presence: true, numericality: { only_integer: true, greater_than: 0 }, length: { is: 9 }
 end
--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -3,6 +3,9 @@ require "test_helper"
 class ContactsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @contact = contacts(:one)
+    @contact_p_string = contacts(:two)
+    @contact_p_tm_numbers = contacts(:three)
+    @contact_p_negative_number = contacts(:four)
   end
 
   test "should get index" do
@@ -68,6 +71,42 @@ class ContactsControllerTest < ActionDispatch::IntegrationTest
   test "should update contact without last_name" do
     patch contact_url(@contact), params: { contact: { first_name: @contact.first_name, last_name: '', phone_number: @contact.phone_number } }
     assert_redirected_to contacts_url
+  end
+
+  test "should not create contact with string phone" do
+    assert_no_difference('Contact.count') do
+      post contacts_url, params: { contact: { first_name: @contact_p_string.first_name, last_name: '', phone_number: @contact_p_string.phone_number } }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "should not update contact with string phone" do
+    patch contact_url(@contact), params: { contact: { first_name: @contact_p_string.first_name, last_name: '', phone_number: @contact_p_string.phone_number } }
+    assert_response :unprocessable_entity
+  end
+
+  test "should not create contact with too many numbers on phone" do
+    assert_no_difference('Contact.count') do
+      post contacts_url, params: { contact: { first_name: @contact_p_tm_numbers.first_name, last_name: '', phone_number: @contact_p_tm_numbers.phone_number } }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "should not update contact with too many numbers on phone" do
+    patch contact_url(@contact), params: { contact: { first_name: @contact_p_tm_numbers.first_name, last_name: '', phone_number: @contact_p_tm_numbers.phone_number } }
+    assert_response :unprocessable_entity
+  end
+
+  test "should not create contact with negative number on phone" do
+    assert_no_difference('Contact.count') do
+      post contacts_url, params: { contact: { first_name: @contact_p_negative_number.first_name, last_name: '', phone_number: @contact_p_negative_number.phone_number } }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "should not update contact with negative number on phone" do
+    patch contact_url(@contact), params: { contact: { first_name: @contact_p_negative_number.first_name, last_name: '', phone_number: @contact_p_negative_number.phone_number } }
+    assert_response :unprocessable_entity
   end
 
 end
